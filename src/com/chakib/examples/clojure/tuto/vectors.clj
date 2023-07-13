377844156ee493919238112a2f9fedf5c4e42cbe
chakib <chakibdaii1995@gmail.com>
2023-07-13 15:58:43 +0200

    cdaii - tutorials - minor fix

--- src/com/chakib/examples/clojure/tuto/vectors.clj
+++ src/com/chakib/examples/clojure/tuto/vectors.clj
@@ -9,7 +9,7 @@
   (println (vec '(1 2 3)))                                  ; returns a vector
   (println (nth [1 "two" 3] 1))                             ; returns the index 1 element in a list
   (println (nth [1 "two" 3] 0))                             ; returns the index 0 element  in a list
-  (println (nth [1 "two" 3] 3))                             ; returns the index 3 element  in a list
+  (println (nth [1 "two" 3] 2))                             ; returns the index 3 element  in a list
   (println [])                                              ; returns an empty list
   )
 
