817c19871264d14e192cdf5b9870e33952e2a4ae
chakib <chakibdaii1995@gmail.com>
2023-09-24 19:08:40 +0200

    cdaii - added anagrams implementations for strings examples

--- src/com/chakib/examples/clojure/tuto/strings.clj
+++ src/com/chakib/examples/clojure/tuto/strings.clj
@@ -1,6 +1,24 @@
 (ns com.chakib.examples.clojure.tuto.strings
   (:require [clojure.string :as string]))
 
+(defn anagrams
+  [str str1]
+  (= (sort str) (sort str1)))
+
+(def prime-numbers [2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 101 103])
+
+(defn str->prime-numbers
+  "mapping a character to a prime number"
+  [str]
+  (map
+    #(nth
+       prime-numbers
+       (- (int %) 97))                                      ; character to ascii - 97
+    (seq str)))
+(defn anagrams-using-primes
+  [^String str ^String str1]
+  (= (reduce * (str->prime-numbers str)) (reduce * (str->prime-numbers str1))))
+
 (defn -main
   [& args]
   (println (format " [x] args %s" args))
@@ -18,5 +36,19 @@
   (println (string/replace "banana and mango" "an" "um"))   ; applying replace
   (println (string/replace "banana" "an" "anan"))           ; applying replace
   (println (.replace (String. "banana")  "an" "anan" ))     ; applying replace in java way
+  (println (anagrams "car" "arc"))
+  (println (anagrams "brag" "grab"))
+  (println (anagrams "no" "test"))
+  (println (map #(int %) (seq "1")))
+  (println (map #(nth prime-numbers (- (int %) 97)) (seq "abc")))
+  (println prime-numbers)
+  (println (nth prime-numbers 1))
+  (println (str->prime-numbers "car"))
+  (println (reduce * (str->prime-numbers "car")))
+  (println (str->prime-numbers "arc"))
+  (println (reduce * (str->prime-numbers "arc")))
+  (println (anagrams-using-primes "car" "arc"))
+  (println (anagrams-using-primes "brag" "grab"))
+  (println (anagrams-using-primes "no" "test"))
   )
 
