c072f0aaf94459fd2b2c0f23dd9341ad20fe39dc
chakib <chakibdaii1995@gmail.com>
2023-09-28 18:58:22 +0200

    cdaii - minor changes

--- src/com/chakib/examples/clojure/tuto/strings.clj
+++ src/com/chakib/examples/clojure/tuto/strings.clj
@@ -3,7 +3,7 @@
 
 (defn anagrams
   [str str1]
-  (= (sort str) (sort str1)))
+  (= (sort (string/lower-case str)) (sort (string/lower-case str1))))
 
 (def prime-numbers [2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 101 103])
 
